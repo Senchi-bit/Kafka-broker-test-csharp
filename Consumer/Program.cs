218baf9b0d3bcfde453a0b271c715514053942a9
Arsen Islyamov <arsenislyam@yandex.ru>
2025-04-29 14:27:54 +0300

    Add Consumer & Producer

---
+++ Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+
+class Consumer
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            
+            GroupId         = "mygroup",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+        const string topic = "sensor_data2";
+        
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) => {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+        
+        using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+        {
+            TopicPartition topicPartition = new TopicPartition(topic, 0);
+            consumer.Assign(topicPartition);
+            try {
+                while (true) {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed event from topic {topic}: key = '0' value = {cr.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException) {
+                // Ctrl-C was pressed.
+            }
+            finally{
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Text.Json;
+
+class Producer
+{
+    static void Main(string[] args)
+    {
+        const string topic = "sensor_data2";
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            Acks = Acks.All
+        };
+
+        TopicPartition topicPartition = new TopicPartition(topic, new Partition(0));
+        using (var producer = new ProducerBuilder<string, string>(config).Build())
+        {
+            while (true)
+            {
+                string message = Console.ReadLine() ?? "Message is null";
+                var messageForTopic = new MessageForTopic(message, topicPartition.Partition.ToString());
+                string jsonValue = JsonSerializer.Serialize(messageForTopic);
+
+                producer.Produce(topicPartition, new Message<string, string> { Key = "0", Value = jsonValue },
+                    (deliveryReport) =>
+                    {
+                        if (deliveryReport.Error.Code != ErrorCode.NoError)
+                        {
+                            Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Produced event to topic {topic}: key = '0' value = {messageForTopic.message}");
+                        }
+                    });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                Console.WriteLine($" Message was produced to topic {topic}");
+            }
+        }
+    }
+}
+
+public record MessageForTopic(string message, string partitionId);
